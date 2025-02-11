eb9ef246e5787912b9c5cbe223805448c1de4c45
thaminiperera <thaminiperera@gmail.com>
2025-02-11 19:08:39 +0530

    feat : editor

---
+++ src/app/documents/page.tsx
@@ -0,0 +1,7 @@
+const DocumentsPage = () => {
+    return ( 
+        <div></div>
+     );
+}
+ 
+export default DocumentsPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <Button>Click me</Button>
+      Click <Link href="http://localhost:3000/documents/123">Here</Link> 
     </div>
   );
 }
