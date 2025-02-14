3f342c34e2d96c70d84335cc775886b60b1127a7
thaminiperera <thaminiperera@gmail.com>
2025-02-14 12:08:24 +0530

    feat ; convex database and documents schema

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,27 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+"use client";
+
+import { useQuery } from "convex/react";
 import { Navbar } from "./navbar";
 import { TemplatesCallery } from "./templates-gallery";
+import { api } from "../../../convex/_generated/api";
 
 export default function Home() {
+  const documents = useQuery(api.documents.get)
+  if (documents === undefined) {
+    return(
+      <p>Loading...</p>
+    )
+  }
   return (
     <div className="flex min-h-screen flex-col ">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
         <Navbar />
       </div>
       <div className="mt-16">
-        <TemplatesCallery/>
+        <TemplatesCallery />
+        {documents?.map((document) => (
+          <span key={document._id}>{document.title}</span>
+        ))}
       </div>
     </div>
   );
